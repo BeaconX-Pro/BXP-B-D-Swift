6e048d8f447749af323a581ed4f9a39dd00e3efd
lovexiaoxia <aadyx2007@163.com>
2026-09-16 18:00:01 +0800

    Initial Commit

---
+++ MKBeaconButtonSwiftProject/MKBeaconButtonSwiftProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MKBeaconButtonSwiftProjectApp.swift
+//  MKBeaconButtonSwiftProject
+//
+//  Created by aa on 2026/9/16.
+//
+
+import SwiftUI
+
+@main
+struct MKBeaconButtonSwiftProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
